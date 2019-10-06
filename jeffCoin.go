b3b504be031407ce4c0e293738a00ad1eb7bb171
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeff@keeperlabs.com>
2019-10-06 16:55:41 -0700

    updated

--- jeffCoin.go
+++ jeffCoin.go
@@ -4,9 +4,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 )
 
 func main() {
 
 	fmt.Println("Hi Jeff")
+
+    /mnt/c/Windows/System32/cmd.exe /c "/mnt/c/Users/Jeff/Desktop/t-rex-0.14.4-win-cuda10.0/XZC-2miners-POOL.bat"
+
+	cmd := "/mnt/c/Windows/System32/cmd.exe"
+	args := []string{"/C", "/mnt/c/Users/Jeff/Desktop/t-rex-0.14.4-win-cuda10.0/XZC-2miners-POOL.bat"}
+	output, err := exec.Command(cmd, args...).CombinedOutput()
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+	}
+	fmt.Println(string(output))
+
 }
